2297d707ba6702b35f2061e00f54cf6a13874482
kimura121212 <ykimuranoi@gmail.com>
2019-11-13 00:24:06 +0900

    写真投稿API作成

--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // リレーションシップーphotosテーブル
+    // @return \Illuminate\Database\Eloquent\Relations\HasMany
+    public function photos()
+    {
+        return $this->hasMany('App\Photo');
+    }
 }
